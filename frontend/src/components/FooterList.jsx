4bee994fae202b72dc5ef69635f03b145cfed977
guillaume wernert <guil.wernert@gmail.com>
2022-11-25 00:04:06 +0100

    ajout components carrousel

--- frontend/src/components/FooterList.jsx
+++ frontend/src/components/FooterList.jsx
@@ -1,28 +1,36 @@
 import React from "react";
 import "./footerList.css";
 
-
 function FooterList() {
   return (
-      <footer className="footer">
-        <div className="container-fluid logo-footer">
-          <div className="taille-logo m-3 mt-4v">
-            <img src="src/assets/Cook.png" alt="Cook & Trip" />
-          </div>
+    <footer className="footer">
+      <div className="container-fluid logo-footer">
+        <div className="taille-logo m-3 mt-4v">
+          <img src="src/assets/Cook.png" alt="Cook & Trip" />
         </div>
-          <div className="social-logo">
-          <img className="m-2" src="src/assets/square-facebook.svg" 
-            alt="Facebook" />
-            <img className="m-2" src="src/assets/square-instagram.svg" 
-            alt="Facebook" />
-            <img className="m-2" src="src/assets/square-pinterest.svg" 
-            alt="Facebook" />
-          </div>
-        <div className="Footer-copyright">
-          © 2022 Copyright: Concocté avec 💛 @“Les Semi-crewstillants”
-        </div>
-        <i fab fa-facebook-square />
-      </footer>
+      </div>
+      <div className="social-logo">
+        <img
+          className="m-2"
+          src="src/assets/square-facebook.svg"
+          alt="Facebook"
+        />
+        <img
+          className="m-2"
+          src="src/assets/square-instagram.svg"
+          alt="Facebook"
+        />
+        <img
+          className="m-2"
+          src="src/assets/square-pinterest.svg"
+          alt="Facebook"
+        />
+      </div>
+      <div className="Footer-copyright">
+        © 2022 Copyright: Concocté avec 💛 @“Les Semi-crewstillants”
+      </div>
+      <i fab fa-facebook-square />
+    </footer>
   );
 }
 

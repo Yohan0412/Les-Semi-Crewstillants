ef2ee810f283ef738dee3d3c28ef876a82ec5c96
guillaume wernert <guil.wernert@gmail.com>
2022-11-24 16:58:26 +0100

    slider home structure

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,74 +4,94 @@ import "../Home.css";
 
 export default function Home() {
   return (
-    <div
-      id="carouselHomeCaptions"
-      className="carousel slide"
-      data-bs-ride="false"
-    >
-      <div className="carousel-indicators">
-        <button
-          type="button"
-          data-bs-target="#carouselHomeCaptions"
-          data-bs-slide-to="0"
-          className="active"
-          aria-current="true"
-          aria-label="Slide 1"
-        />
-        <button
-          type="button"
-          data-bs-target="#carouselHomeCaptions"
-          data-bs-slide-to="1"
-          aria-label="Slide 2"
-        />
-        <button
-          type="button"
-          data-bs-target="#carouselHomeCaptions"
-          data-bs-slide-to="2"
-          aria-label="Slide 3"
-        />
-      </div>
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src="..." className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img src="..." className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Some representative placeholder content for the second slide.</p>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img src="..." className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-          </div>
-        </div>
-      </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselHomeCaptions"
-        data-bs-slide="prev"
+    <div className="home">
+      <div
+        id="carouselHomeCaptions"
+        className="carousel slide"
+        data-bs-ride="false"
       >
-        <span className="carousel-control-prev-icon" aria-hidden="true" />
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselHomeCaptions"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true" />
-        <span className="visually-hidden">Next</span>
-      </button>
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselHomeCaptions"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          />
+          <button
+            type="button"
+            data-bs-target="#carouselHomeCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          />
+          <button
+            type="button"
+            data-bs-target="#carouselHomeCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          />
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="http://ileauxepices.com/blog/wp-content/uploads/2016/11/poulet-curry-vert-thailande.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://s3-eu-west-1.amazonaws.com/images-ca-1-0-1-eu/recipe_photos/original/632/nems-3000x2000.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://www.royalchill.com/wp-content/uploads/2019/02/recette-facile-poulet-tikka.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselHomeCaptions"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselHomeCaptions"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
   );
 }

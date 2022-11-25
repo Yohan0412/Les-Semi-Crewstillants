3496b6e9fb5c87301c2cef80020a6425b3ccd027
SEBASTIEN_ARTESI <SEBASTIEN.ARTESI@gmail.com>
2022-11-25 04:43:51 +0100

    retouche Homepage, footer, ajout photos

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -37,41 +37,41 @@ export default function Home() {
           <div className="carousel-inner">
             <div className="carousel-item active">
               <img
-                src="http://ileauxepices.com/blog/wp-content/uploads/2016/11/poulet-curry-vert-thailande.jpg"
+                src="src/assets/nan.jpg"
                 className="d-block w-100"
                 alt="..."
               />
               <div className="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
+                {/* <h5>First slide label</h5>
                 <p>
                   Some representative placeholder content for the first slide.
-                </p>
+                </p> */}
               </div>
             </div>
             <div className="carousel-item">
               <img
-                src="https://s3-eu-west-1.amazonaws.com/images-ca-1-0-1-eu/recipe_photos/original/632/nems-3000x2000.jpg"
+                src="src/assets/japan.jpg"
                 className="d-block w-100"
                 alt="..."
               />
               <div className="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
+                {/* <h5>Second slide label</h5>
                 <p>
                   Some representative placeholder content for the second slide.
-                </p>
+                </p> */}
               </div>
             </div>
             <div className="carousel-item">
               <img
-                src="https://www.royalchill.com/wp-content/uploads/2019/02/recette-facile-poulet-tikka.jpg"
+                src="src/assets/indian.jpg"
                 className="d-block w-100"
                 alt="..."
               />
               <div className="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
+                {/* <h5>Third slide label</h5>
                 <p>
                   Some representative placeholder content for the third slide.
-                </p>
+                </p> */}
               </div>
             </div>
           </div>
@@ -105,42 +105,42 @@ export default function Home() {
           </video>
         </div>
         <div className="projetPresentation">
-          <h2>Notre projet</h2>
+          <h2>Cook & Trip</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-            architecto, delectus molestias modi deserunt at, harum dolores
-            placeat inventore, sequi minima accusamus pariatur nisi voluptatibus
-            deleniti? Odio quia aspernatur possimus!
+            Si comme nous vous aimez les bons plats, en famille, entre amies ou encore seul (n'ayez pas honte d'être gourmand😜) <br />
+            alors montez à bord de notre Combi sillonner les routes de la cuisine du monde avec nous. <br />
+            L'idée ?! Vous faire découvrir de nouvelle saveur, de nouvelle destination, émoustiller vos papilles, époustoufler vos invités ! <br />
+            Alors ?! On y va ?! 🚐
           </p>
         </div>
         <div className="continentHome d-flex flex-column">
           <div className="america d-flex gap-1 w-100">
             <img
-              src="https://i.ebayimg.com/images/g/YnoAAOSwcvdjC41I/s-l400.jpg"
+              src="src/assets/North_America.jpg"
               alt=""
               className="northamerica w-50"
             />
             <img
-              src="https://www.seanews.com.tr/images/haberler/2016_05/161140/latin-america1.jpg"
+              src="src/assets/South_America.jpg"
               alt=""
               className="southAmerica w-50"
             />
           </div>
           <img
-            src="https://asset-a.grid.id//crop/0x0:0x0/360x240/photo/2022/06/07/benua-asiajpg-20220607093406.jpg"
+            src="src/assets/African.jpg"
             alt=""
             className="asia w-100 py-1"
           />
           <div className="africaEurope d-flex w-100 gap-1">
             <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQomdm7APGFSTwXby-p1XZbLERBKS7jOuFgWauNubOa1upgyZlcaq1VE5rV3BzqOfBQsto&usqp=CAU"
+              src="src/assets/Asian.jpg"
               alt=""
-              className="africa w-45"
+              className="africa w-50"
             />
             <img
-              src="https://voyage.aprr.fr/sites/default/files/styles/content_image/public/2022-06/Carte%20itin%C3%A9raire%202019_V9%20%281%29.jpg?itok=AtirYDPG"
+              src="src/assets/Europe.jpg"
               alt=""
-              className="europe w-75"
+              className="europe w-50"
             />
           </div>
         </div>

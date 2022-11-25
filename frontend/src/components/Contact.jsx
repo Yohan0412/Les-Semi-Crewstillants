e94d63d82efdcc03e4eda22e96a3b946752393d3
PETACCIA <petaccia.seb@gmail.com>
2022-11-25 10:39:24 +0100

    formulaire fais en responsive

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,58 +1,66 @@
 import React from "react";
+import "./contact.css";
 
 function Contact() {
   return (
-    <form className="form container-fluid bg-success">
-      <div className="">
-        <input
-          value=""
-          className="input-name w-50 m-1 p-2"
-          type="text"
-          name="user_name"
-          placeholder="Nom"
-          required
-        />
-      </div>
+    <form className="form ">
+      <img
+        src="https://media.discordapp.net/attachments/1044340376091234378/1045510021292769341/pexels-pixabay-326279.jpg"
+        className="img-fluid"
+        alt="fond du hearder"
+      />
+      <div className="container-fluid bg-success">
+        <div className="contact-name">
+          <input
+            value=""
+            className="input-name m-1 p-2"
+            type="text"
+            name="user_name"
+            placeholder="Nom"
+            required
+          />
+        </div>
 
-      <div className="">
-        <input
-          value=""
-          className="input-name w-50 m-1 p-2"
-          type="text"
-          name="prenom"
-          placeholder="Prénom"
-          required
-        />
-      </div>
+        <div className="">
+          <input
+            value=""
+            className="input-name m-1 p-2"
+            type="text"
+            name="prenom"
+            placeholder="Prénom"
+            required
+          />
+        </div>
 
-      <div className="">
-        <input
-          value=""
-          className="input-email w-50 m-1 p-2"
-          type="email"
-          name="user_email"
-          placeholder="Adress email"
-          required
-        />
-      </div>
+        <div className="">
+          <input
+            value=""
+            className="input-email  m-1 p-2"
+            type="email"
+            name="user_email"
+            placeholder="Adresse email"
+            required
+          />
+        </div>
 
-      <div className="">
-        <textarea
-          value=""
-          className="contactTextarea  w-50 m-1 p-2"
-          name="message"
-          placeholder="Enter your message"
-          required
-        />
-      </div>
+        <div className="">
+          <textarea
+            value=""
+            className="contactTextarea m-1 p-2"
+            name="message"
+            placeholder="Entrer votre message"
+            required
+          />
+        </div>
 
-      <div className="">
-        <button
-          className="button w-25 fs-5 bg-warning text-success"
-          type="submit"
-        >
-          submit
-        </button>
+        <div className="">
+          <button
+            className="contactFormButton w-25  bg-warning text-success m-3"
+            type="submit"
+          >
+            Envoyer
+          </button>
+        </div>
       </div>
     </form>
   );
